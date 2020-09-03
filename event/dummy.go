f82a59ee8a4a95c00e638cc861a941d0b6b41acc
Szymon Gibala <szymon.gibala@sap.com>
2020-09-03 11:34:59 +0200

    Add dummy *.go file to fix vendoring

---
+++ event/dummy.go
@@ -0,0 +1,11 @@
+// +build dummy
+
+// This file is part of a workaround for `go mod vendor` which won't
+// vendor C files if there are no Go files in the same directory.
+// This prevents the C header files in event/ from being vendored.
+//
+// These two file make it so `go mod vendor` behaves correctly.
+//
+// See this issue for reference: https://github.com/golang/go/issues/26366
+
+package event
